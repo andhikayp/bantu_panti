802ef7ddef49c68d798723812b70e438175d6d66
Andhika <andhikay24@gmail.com>
2019-05-14 10:52:38 +0700

    profil (anak panti && donatur)

--- application/views/dashboard/profil/petugas/edit.slice.php
+++ application/views/dashboard/profil/petugas/edit.slice.php
@@ -24,14 +24,15 @@
     @endif
 </div>
 
+@if($this->session->user_login['id_role'] == '3')
 <nav class="breadcrumb bg-white push">
     <a class="breadcrumb-item" href="{{ base_url('/') }}">Dashboard</a>
-    <a class="breadcrumb-item" href="{{ base_url('PetugasController/indexProfil') }}">Profil Petugas</a>
-    <span class="breadcrumb-item active">Edit Profil Petugas</span>
+    <a class="breadcrumb-item" href="{{ base_url('PetugasController/indexProfil') }}">Profil Donatur</a>
+    <span class="breadcrumb-item active">Edit Profil Donatur</span>
 </nav>
 <div class="block">
     <div class="block-header block-header-default bg-primary">
-        <h3 class="block-title">Edit Profil Petugas</h3>
+        <h3 class="block-title">Edit Profil Donatur</h3>
     </div>
     <div class="block-content">
         <form class="js-validation-signup px-30" method="POST" enctype="multipart/form-data" action="{{ base_url('PetugasController/editProfil') }}" aria-label="">
@@ -39,7 +40,7 @@
                 <div class="col-12">
                     <div class="form-material form-material-primary floating input-group">
                         <input value="{{ $petugas->nama_petugas }}" type="text" class="form-control" id="nama_petugas" name="nama_petugas">
-                        <label for="nama_petugas">Nama Petugas</label>
+                        <label for="nama_petugas">Nama Donatur</label>
                         <div class="input-group-append">
                             <span class="input-group-text">Contoh: Hikmawan</span>
                         </div>
@@ -50,7 +51,7 @@
                 <div class="col-12">
                     <div class="form-material form-material-primary floating input-group">
                         <input value="{{ $petugas->email_petugas }}" type="text" class="form-control" id="email_petugas" name="email_petugas">
-                        <label for="email_petugas">Email Petugas</label>
+                        <label for="email_petugas">Email Donatur</label>
                         <div class="input-group-append">
                             <span class="input-group-text">Contoh: hikmawan@gmail.com</span>
                         </div>
@@ -61,7 +62,7 @@
                 <div class="col-12">
                     <div class="form-material form-material-primary floating input-group">
                         <input value="{{ $petugas->no_hape_petugas }}" type="text" class="form-control" id="no_hape_petugas" name="no_hape_petugas">
-                        <label for="no_hape_petugas">Nomer Telepon Petugas</label>
+                        <label for="no_hape_petugas">Nomer Telepon Donatur</label>
                         <div class="input-group-append">
                             <span class="input-group-text">Contoh: 085850000000</span>
                         </div>
@@ -72,7 +73,7 @@
                 <div class="col-12">
                     <div class="form-material form-material-primary floating input-group">
                         <input value="{{ $petugas->alamat_petugas }}" type="text" class="form-control" id="alamat_petugas" name="alamat_petugas">
-                        <label for="alamat_petugas">Alamat Petugas</label>
+                        <label for="alamat_petugas">Alamat Donatur</label>
                         <div class="input-group-append">
                             <span class="input-group-text">Contoh: Desa Rejeni RT 12 RW 06 Krembung-Sidoarjo</span>
                         </div>
@@ -88,4 +89,72 @@
         </form>
     </div>
 </div>
+@endif
+@if($this->session->user_login['id_role'] == '4')
+<nav class="breadcrumb bg-white push">
+    <a class="breadcrumb-item" href="{{ base_url('/') }}">Dashboard</a>
+    <a class="breadcrumb-item" href="{{ base_url('PetugasController/indexProfil') }}">Profil Anak Panti</a>
+    <span class="breadcrumb-item active">Edit Profil Anak Panti</span>
+</nav>
+<div class="block">
+    <div class="block-header block-header-default bg-primary">
+        <h3 class="block-title">Edit Profil Petugas</h3>
+    </div>
+    <div class="block-content">
+        <form class="js-validation-signup px-30" method="POST" enctype="multipart/form-data" action="{{ base_url('PetugasController/editProfil') }}" aria-label="">
+            <div class="form-group row">
+                <div class="col-12">
+                    <div class="form-material form-material-primary floating input-group">
+                        <input value="{{ $petugas->nama_petugas }}" type="text" class="form-control" id="nama_petugas" name="nama_petugas">
+                        <label for="nama_petugas">Nama Anak Panti</label>
+                        <div class="input-group-append">
+                            <span class="input-group-text">Contoh: Hikmawan</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-12">
+                    <div class="form-material form-material-primary floating input-group">
+                        <input value="{{ $petugas->email_petugas }}" type="text" class="form-control" id="email_petugas" name="email_petugas">
+                        <label for="email_petugas">Email Anak Panti</label>
+                        <div class="input-group-append">
+                            <span class="input-group-text">Contoh: hikmawan@gmail.com</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-12">
+                    <div class="form-material form-material-primary floating input-group">
+                        <input value="{{ $petugas->no_hape_petugas }}" type="text" class="form-control" id="no_hape_petugas" name="no_hape_petugas">
+                        <label for="no_hape_petugas">Nomer Telepon Anak Panti</label>
+                        <div class="input-group-append">
+                            <span class="input-group-text">Contoh: 085850000000</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-12">
+                    <div class="form-material form-material-primary floating input-group">
+                        <input value="{{ $petugas->alamat_petugas }}" type="text" class="form-control" id="alamat_petugas" name="alamat_petugas">
+                        <label for="alamat_petugas">Alamat Anak Panti</label>
+                        <div class="input-group-append">
+                            <span class="input-group-text">Contoh: Desa Rejeni RT 12 RW 06 Krembung-Sidoarjo</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr/><div class="row mb-2">
+                <div class="col-3"></div>
+                    <a class="col-3 btn btn-danger" href="{{ base_url('PetugasController/indexProfil') }}">Cancel</a>&nbsp
+                    <button type="submit" class="col-3 btn bg-earth text-white">Submit</button>
+                <div class="col-3"></div>
+            </div>
+        </form>
+    </div>
+</div>
+@endif
+
 @endsection
--- application/views/dashboard/profil/petugas/index.slice.php
+++ application/views/dashboard/profil/petugas/index.slice.php
@@ -23,14 +23,14 @@
         @endif
     @endif
 </div>
-
+@if($this->session->user_login['id_role'] == '3')
 <nav class="breadcrumb bg-white push">
     <a class="breadcrumb-item" href="{{ base_url('/') }}">Dashboard</a>
-    <span class="breadcrumb-item active">Profil Petugas</span>
+    <span class="breadcrumb-item active">Profil Donatur</span>
 </nav>
 <div class="block block-themed">
     <div class="block-header bg-gd-lake">
-        <h3 class="block-title">Profil Petugas</h3>
+        <h3 class="block-title">Profil Donatur</h3>
         <div class="block-options">
             <a href="{{ base_url('PetugasController/editProfil') }}"><button type="button" class="btn-block-option btn-sm bg-danger">
                 <i class="si si-pencil"></i> Ubah
@@ -41,19 +41,19 @@
         <div class="row">
             <div class="col-6">
                 <div class="form-group row">
-                    <label class="col-12">Username Petugas</label>
+                    <label class="col-12">Username Donatur</label>
                     <div class="col-md-9">
                         <div class="form-control-plaintext"><i class="fa fa-arrow-right mr-5"></i><b>{{ $petugas->username ? $petugas->username : "-" }}</b></div>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-12">Nama Petugas</label>
+                    <label class="col-12">Nama Donatur</label>
                     <div class="col-md-9">
                         <div class="form-control-plaintext"><i class="fa fa-arrow-right mr-5"></i><b>{{ $petugas->nama_petugas ? $petugas->nama_petugas : "-" }}</b></div>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-12">Email Petugas</label>
+                    <label class="col-12">Email Donatur</label>
                     <div class="col-md-9">
                         <div class="form-control-plaintext"><i class="fa fa-arrow-right mr-5"></i><b>{{ $petugas->email_petugas ? $petugas->email_petugas : "-" }}</b></div>
                     </div>
@@ -61,13 +61,13 @@
             </div>
             <div class="col-6">
                 <div class="form-group row">
-                    <label class="col-12">Nomer Telepon Petugas</label>
+                    <label class="col-12">Nomer Telepon Donatur</label>
                     <div class="col-md-9">
                         <div class="form-control-plaintext"><i class="fa fa-arrow-right mr-5"></i><b>{{ $petugas->no_hape_petugas ? $petugas->no_hape_petugas : "-" }}</b></div>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-12">Alamat Petugas</label>
+                    <label class="col-12">Alamat Donatur</label>
                     <div class="col-md-9">
                         <div class="form-control-plaintext"><i class="fa fa-arrow-right mr-5"></i><b>{{ $petugas->alamat_petugas ? $petugas->alamat_petugas : "-" }}</b></div>
                     </div>
@@ -76,4 +76,59 @@
         </div>
     </div>
 </div>
+@endif
+@if($this->session->user_login['id_role'] == '4')
+<nav class="breadcrumb bg-white push">
+    <a class="breadcrumb-item" href="{{ base_url('/') }}">Dashboard</a>
+    <span class="breadcrumb-item active">Profil Anak Panti</span>
+</nav>
+<div class="block block-themed">
+    <div class="block-header bg-gd-lake">
+        <h3 class="block-title">Profil Anak Panti</h3>
+        <div class="block-options">
+            <a href="{{ base_url('PetugasController/editProfil') }}"><button type="button" class="btn-block-option btn-sm bg-danger">
+                <i class="si si-pencil"></i> Ubah
+            </button></a>
+        </div>
+    </div>
+    <div class="block-content">
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group row">
+                    <label class="col-12">Username Anak Panti</label>
+                    <div class="col-md-9">
+                        <div class="form-control-plaintext"><i class="fa fa-arrow-right mr-5"></i><b>{{ $petugas->username ? $petugas->username : "-" }}</b></div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-12">Nama Anak Panti</label>
+                    <div class="col-md-9">
+                        <div class="form-control-plaintext"><i class="fa fa-arrow-right mr-5"></i><b>{{ $petugas->nama_petugas ? $petugas->nama_petugas : "-" }}</b></div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-12">Email Anak Panti</label>
+                    <div class="col-md-9">
+                        <div class="form-control-plaintext"><i class="fa fa-arrow-right mr-5"></i><b>{{ $petugas->email_petugas ? $petugas->email_petugas : "-" }}</b></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="form-group row">
+                    <label class="col-12">Nomer Telepon Anak Panti</label>
+                    <div class="col-md-9">
+                        <div class="form-control-plaintext"><i class="fa fa-arrow-right mr-5"></i><b>{{ $petugas->no_hape_petugas ? $petugas->no_hape_petugas : "-" }}</b></div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-12">Alamat Anak Panti</label>
+                    <div class="col-md-9">
+                        <div class="form-control-plaintext"><i class="fa fa-arrow-right mr-5"></i><b>{{ $petugas->alamat_petugas ? $petugas->alamat_petugas : "-" }}</b></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endif
 @endsection
